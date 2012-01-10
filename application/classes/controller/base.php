fe698997e51e1bf9eea0cafd349973d1c4744904
xsen <troi007@gmail.com>
2012-01-10 19:22:02 +0600

    init top Menu

--- application/classes/controller/base.php
+++ application/classes/controller/base.php
@@ -3,21 +3,39 @@
 class Controller_Base extends Controller {
 
 	protected $_base_view = 'main';
-	protected $_js_file   = array();
-	protected $_css_file  = array();
+
+	protected $_js_file   = array('_' => "\n");
+	protected $_css_file  = array('_' => "\n");
+
+	protected $_top_menu = array(
+		'Home' => '#',
+		'Diary' => '#',
+		'Books' => '#',
+	);
+
+	protected $_this_menu_item = 'Home';
 
 	public function before() {
+
 		$this->view = View::factory($this->_base_view);
+		$this->attach_file('design/bootstrap/bootstrap.min.css');
 	}
 
 	public function after() {
 
-		$this->view->_css_file = implode(' \n ',$this->_css_file);
+		$this->view->_css_file = implode(" \n ",$this->_css_file);
+		$this->view->_js_file  = implode(" \n ",$this->_js_file);
+
+		$this->view->top_menu  = $this->_top_menu;
+		$this->view->menu_item = $this->_this_menu_item;
+
 		$this->response->body($this->view);
 	}
 
 	public function action_index() {
-		$this->attach_file('design/bootstrap/bootstrap.min.css');
+		$view = View::factory('auth');
+
+		$this->view->content = $view;
 	}
 
 	protected function attach_file($file_path) {
@@ -31,7 +49,7 @@ class Controller_Base extends Controller {
 			}
 
 			case 'js' : {
-
+			$this->_js_file[] = '<script type="text/javascript" src="'.URL::base().$file_path.'"></script> ';
 			}
 
 			default : {
---
+++ application/views/auth.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: troy
+ * Date: 10.01.12
+ * Time: 18:17
+ * To change this template use File | Settings | File Templates.
+ */
