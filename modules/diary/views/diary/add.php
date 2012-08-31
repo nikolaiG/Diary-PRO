33d3b4d9be86a0a7940def3dc6029fa0b0151b4e
xsen <evgeny@sitesoft.ru>
2012-08-31 17:05:05 +0600

    add post view

--- modules/diary/views/diary/add.php
+++ modules/diary/views/diary/add.php
@@ -1,3 +1,18 @@
-<textarea rows="" cols="" name="text">
+<div class="row-fluid">
+    <div class="well">
+        <form>
+            <legend>Добавить пост</legend>
+            <label>Заголовок</label>
+            <input type="text" name="name" class="input-large" placeholder="Заголовок">
 
-</textarea>
+            <textarea name="text" style="width: 810px; height: 200px" placeholder="Введите текст">
+            </textarea>
+
+            <label class="checkbox">
+                <input type="checkbox"> Check me out
+            </label>
+            <button type="submit" class="btn btn-success">Submit</button>
+
+        </form>
+    </div>
+</div>
